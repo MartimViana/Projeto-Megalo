d7196bdc4ab579f6209d39313c0ca2b75a31f4c8
Lain <martimviana98@gmail.com>
2018-06-18 22:27:39 +0100

    Adicionei umas classes placeholder

---
+++ src/Screen.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author André Alves
+ *
+ */
+public class Screen {
+	private int height, width;
+	private String name;
+	//TOADD swing stuff
+	public Screen(int height, int width, String name) {
+		this.height = height;
+		this.width = width;
+		this.name = name;
+		//TODO swing stuff
+	}
+}
